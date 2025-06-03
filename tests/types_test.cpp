0255d093df6e14db46a333272f91d2f485a0a794
OlivierGisselmann <gisselmann.olivier@gmail.com>
2025-06-03 17:21:43 +0200

    feat: add all numeric typedef tests

--- tests/types_test.cpp
+++ tests/types_test.cpp
@@ -11,7 +11,16 @@ int main(int argc, char** argv)
 
 TEST(TypesTest, CheckPrimitiveSize)
 {
+    ASSERT_EQ(sizeof(i8), 1);
+    ASSERT_EQ(sizeof(i16), 2);
     ASSERT_EQ(sizeof(i32), 4);
-    ASSERT_EQ(sizeof(f64), 8);
+    ASSERT_EQ(sizeof(i64), 8);
+
+    ASSERT_EQ(sizeof(u8), 1);
     ASSERT_EQ(sizeof(u16), 2);
+    ASSERT_EQ(sizeof(u32), 4);
+    ASSERT_EQ(sizeof(u64), 8);
+
+    ASSERT_EQ(sizeof(f32), 4);
+    ASSERT_EQ(sizeof(f64), 8);
 }

7ce52ad5eb81785e0677183a427e7b3fe7ff46f4
OlivierGisselmann <gisselmann.olivier@gmail.com>
2025-06-26 08:13:10 +0200

    feat: add assert macro

---
+++ engine/core/assert.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#define ASSERT(expr)                                                \
+    do {                                                            \
+        if (!(expr)) {                                              \
+            std::cerr << "Assertion failed: (" << #expr << "), "    \
+                      << "function " << __FUNCTION__                \
+                      << ", file " << __FILE__                      \
+                      << ", line " << __LINE__ << "." << std::endl; \
+            std::abort();                                           \
+        }                                                           \
+    } while (0)

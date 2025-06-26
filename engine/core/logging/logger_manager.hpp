b072cb89a571f305c8321f82037bf7e18a54d8b3
OlivierGisselmann <gisselmann.olivier@gmail.com>
2025-06-26 11:01:44 +0200

    feat: add stream logging macro

--- engine/core/logging/logger_manager.hpp
+++ engine/core/logging/logger_manager.hpp
@@ -7,6 +7,9 @@
 #define LOG(level, msg) \
     Mnemos::LoggerManager::Get().Log(level, msg, __FILE__, __LINE__, __FUNCTION__)
 
+#define LOGF(level) \
+    LogStream(level, __FILE__, __LINE__, __FUNCTION__).Stream()
+
 namespace Mnemos
 {
     class LoggerManager : public ISubsystem
@@ -20,5 +23,25 @@ namespace Mnemos
             static DefaultLogger sLogger;
             return sLogger;
         }
+    };
+
+    class LogStream
+    {
+    public:
+        LogStream(LogLevel level, const char* file, int line, const char* func)
+            : level(level), file(file), line(line), func(func) {}
+
+        ~LogStream() {
+            LoggerManager::Get().Log(level, oss.str(), file, line, func);
+        }
+
+        std::ostringstream& Stream() { return oss; }
+
+    private:
+        std::ostringstream oss;
+        LogLevel level;
+        const char* file;
+        int line;
+        const char* func;
     };
 }
